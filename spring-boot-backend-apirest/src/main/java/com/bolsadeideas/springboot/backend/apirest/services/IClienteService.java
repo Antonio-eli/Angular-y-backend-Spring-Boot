d17bc5079a02ac0ac0b30b9bd2ccdd8af7551d39
Antonio-eli <eliceoroan@hotmail.com>
2022-05-31 23:48:33 -0500

    Agregando paginacion en nuestra Api Rest con Spring Data JPA

--- spring-boot-backend-apirest/src/main/java/com/bolsadeideas/springboot/backend/apirest/services/IClienteService.java
+++ spring-boot-backend-apirest/src/main/java/com/bolsadeideas/springboot/backend/apirest/services/IClienteService.java
@@ -2,10 +2,16 @@ package com.bolsadeideas.springboot.backend.apirest.services;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import com.bolsadeideas.springboot.backend.apirest.models.entity.Cliente;
 
 public interface IClienteService {
+	
 	public List<Cliente> FindAll();
+	
+	public Page<Cliente> FindAll(Pageable pageable);
 	
 	public Cliente findById(Long id);
 	
